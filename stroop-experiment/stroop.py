55ab6dc6c7f8c3d89942a8e1a987a1079a1fe2ff
Yoonwon Jung <yoonwonjung12@gmail.com>
2025-02-15 22:08:58 -0800

    Part1-feedback-correction-and-timeout

--- stroop-experiment/stroop.py
+++ stroop-experiment/stroop.py
@@ -2,6 +2,7 @@ import time
 import sys
 import os
 import random
+import numpy as np
 from psychopy import visual,event,core,gui
 
 stimuli = ['red', 'orange', 'yellow', 'green', 'blue']
@@ -12,6 +13,7 @@ word_stim = visual.TextStim(win,text="", height=40, color="black",pos=[0,0])
 instruction = visual.TextStim(win,text="Press the first letter of the ink color", height=20, color="black",pos=[0,-200])
 fixation_cross = visual.TextStim(win, text="+", height=15, color="black", pos=[0,0]) #set fixation cross: set height as 15 and color as black (as indicated in the instruction)
 feedback_incorrect= visual.TextStim(win,text="Incorrect", height=20, color="black", pos=[0,0]) #this line was already added in "Part1-RT-correction2", and therefore did not show up on "Part1-feedback" commit.
+feedback_slow= visual.TextStim(win,text="Too slow", height=20, color="black", pos=[0,0])
 
 instruction.autoDraw = True
 
@@ -34,19 +36,31 @@ while True:
     word_stim.draw() #draw placeholder, instruction, and stiuli
     win.flip()
     timer.reset() #start timing=reset the timer(right after we draw word_stim)
-    keypress= event.waitKeys(keyList=['r','o','y','g','b','q'])
-    cur_rt= timer.getTime() #get the RT (for button press)
+    keypress= event.waitKeys(keyList=['r','o','y','g','b','q'],maxWait=2)
+    if keypress: #if key was pressed
+        if keypress[0]=='q': #if the key pressed was q
+            # do not store cur_rt to RT
+            print(RT) # print to make sure the response time for pressing 'q' was not stored to the list
+            win.close()
+            core.quit()
+        else: #if the key pressed was not q
+            cur_rt= timer.getTime() #get response time (for button press)
+            if keypress[0]!=cur_stim[0]: #if the key pressed is incorrect
+                feedback_incorrect.draw() #incorrect feedback
+                win.flip()
+                core.wait(1)
+            else: #if it is correct
+                pass
+            RT.append(cur_rt) #append cur_rt to RT only when the key pressed is not 'q'
+            print(RT)
+    else: #if the key was not pressed
+        cur_rt= np.nan #if it gets timeout, store cur_rt value as np.nan value (to prevent messing up the order of response time appended to RT)
     #print(keypress)
-    if keypress[0]=='q':
+        RT.append(cur_rt) #append nan value (for missing RT due to timeout)
         print(RT)
-        win.close()
-        core.quit()
-    else:
-        if keypress[0]!=cur_stim:
-            feedback_incorrect.draw()
-            win.flip()
-            core.wait(1)
-        RT.append(cur_rt) #append cur_rt to RT only when the key pressed is not 'q'
-        print(RT)
+        feedback_slow.draw()
+        win.flip()
+        core.wait(1)
+
 
 
